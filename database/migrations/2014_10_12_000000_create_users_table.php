b5ecfc246641b5b79b54f50ccd6525d094f78931
Fabian Paredes <f.paredesb92@gmail.com>
2023-06-27 21:52:20 -0500

    columna para identificar quien creo al usuario

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('created_by')->nullable();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('avatar_name')->nullable();
